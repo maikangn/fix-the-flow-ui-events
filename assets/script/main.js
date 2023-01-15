745001f8f900f23d14d5f09437e5835d1aef1b79
MAIKA <maikanguyenn@gmail.com>
2023-01-15 19:24:16 +0100

    color change animation

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,12 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+// Color change Design button
+let changeColor = document.querySelector('a:nth-of-type(2)')
+
+changeColor.addEventListener('click', show)
+
+function show(){
+  changeColor.classList.toggle('show')
+}
