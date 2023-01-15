2b3f20e2661b0ea395dd99e3dca0f8c4a966549c
MAIKA <maikanguyenn@gmail.com>
2023-01-15 19:39:03 +0100

    button enlargement animation

--- assets/script/main.js
+++ assets/script/main.js
@@ -7,7 +7,17 @@ function jumpHandler() {
   interaction.classList.toggle('jump')
 }
 
-// Color change Design button
+// 1. Enlarging Frontend button
+let bigger = document.querySelector('a:nth-of-type(1)')
+
+bigger.addEventListener('click', larger)
+bigger.addEventListener('animationed', larger)
+
+function larger() {
+bigger.classList.toggle('enlarge')
+}
+
+// 2.Color change Design button
 let changeColor = document.querySelector('a:nth-of-type(2)')
 
 changeColor.addEventListener('click', show)
